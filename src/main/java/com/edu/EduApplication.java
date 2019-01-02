4609e2f05a3f4b27d17fe6b52e5b656678e72703
Nick Lukashchuk <nm.luka@gmail.com>
2019-01-02 21:21:20 +0200

    init commit

--- src/main/java/com/edu/EduApplication.java
+++ src/main/java/com/edu/EduApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EduApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(EduApplication.class, args);
         System.out.println("Hello world!!!");
     }
